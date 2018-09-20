7d7ab7dbcc38103aa7ef240ade5234a9a65a29df
Kapil <kkhanal16@winona.edu>
2018-09-20 10:38:34 -0500

    select function added

--- tabletoolz/tabletoolz.py
+++ tabletoolz/tabletoolz.py
@@ -1,3 +1,11 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+
+def select(df,cols):
+    '''Input: Dataframe as list of dictionary, each dictionary as a row and list of cols to select
+       Output:list of dictionary, each dictionary as a row with selected column from cols '''
+    for row in df:
+        for k,v in row.items():
+            if k in cols:
+                yield({k:v})
