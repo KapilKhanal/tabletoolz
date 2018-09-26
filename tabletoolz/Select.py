4d1ede6c3668a517df567ca7b12496df641d7b6f
Kapil <kkhanal16@winona.edu>
2018-09-26 09:59:00 -0500

    made it generator and fixed a logical error
    now returns a entire row for matching column at a time instead of just value intersection of row and column

--- tabletoolz/Select.py
+++ tabletoolz/Select.py
@@ -1,21 +1,15 @@
-
-# coding: utf-8
-
-# In[ ]:
-
-
 df = [{"Name":"Kapil","Major":"Data"},{"Name":"Mapil","Major":"Hata"}]
 cols = ["Name"]
 
 def select_yield(df,cols):
     '''Input: Dataframe as list of dictionary, each dictionary as a row and list of cols to select
        Output:list of dictionary, each dictionary as a row with selected column from cols '''
-    for row in df:
-        for k,v in row.items():
-            if k in cols:
-                yield({k:v})
+    yield ({k:v for k,v in row.items() if k in cols} for row in df)
+    
+    
+                
                 
 
-select = lambda df,cols: ({k:v} for row in df for k,v in row.items() if k in cols)
-list(select(df,["Name"]))
+select = lambda df,cols: ({k:v for k,v in row.items() if k in cols} for row in df)
+list(next(select_yield(df,["Name","Major"])))
 
